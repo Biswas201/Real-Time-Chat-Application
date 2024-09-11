aafb433340ff8762ff7c67e080fb31480ea8ce1c
anurup <anurupbiswas143@gmail.com>
2024-09-11 19:42:53 +0530

    Database

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,20 @@
-
 import express from "express"
+import mongoose from "mongoose"
 import dotenv from "dotenv"
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
+
+//database connect
+mongoose
+  .connect(process.env.Database_Uri)
+  .then(() => {
+    console.log("Database is connected")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 const app = express();
 

6ee6e577840e722f865ca209609389fd73d54d66
anurup <anurupbiswas143@gmail.com>
2024-09-13 19:39:42 +0530

    edit2

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,7 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
-// import { errorHandler } from "../Utils/error.js";
+import { errorHandler } from "../Utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password, confirmPassword, gender } = req.body;
@@ -11,16 +11,11 @@ export const signup = async (req, res, next) => {
   validUser = await User.findOne({ email });
 
   if (validUser) {
-    return res.status(400).json({
-      success: false,
-      message: "User already exist",
-    });
+    return next(errorHandler(400, "User already exists"));
   }
 
   if (password !== confirmPassword) {
-    return res.status(400).json({
-      error: "Password don't match",
-    });
+    return next(errorHandler(400, "Password don't match"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);

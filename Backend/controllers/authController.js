b74e90e0dc227b8d4c252222e504a8e85f726098
anurup <anurupbiswas143@gmail.com>
2024-09-13 19:59:21 +0530

    login function

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -51,34 +51,34 @@ export const signup = async (req, res, next) => {
   }
 };
 
-// export const login = async (req, res, next) => {
-//   try {
-//     const { email, password } = req.body
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body
 
-//     const validUser = await User.findOne({ email })
+    const validUser = await User.findOne({ email })
 
-//     if (!validUser) {
-//       return next(errorHandler(404, "User not found"))
-//     }
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"))
+    }
 
-//     const validPassword = bcryptjs.compareSync(password, validUser.password)
+    const validPassword = bcryptjs.compareSync(password, validUser.password)
 
-//     if (!validPassword) {
-//       return next(errorHandler(401, "Wrong Credentials"))
-//     }
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credentials"))
+    }
 
-//     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
 
-//     res.cookie("access_token", token, { httpOnly: true }).status(200).json({
-//       _id: validUser._id,
-//       username: validUser.username,
-//       email: validUser.email,
-//       profilePic: validUser.profilePic,
-//     })
-//   } catch (error) {
-//     next(error)
-//   }
-// }
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json({
+      _id: validUser._id,
+      username: validUser.username,
+      email: validUser.email,
+      profilePic: validUser.profilePic,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
 
 // export const logout = (req, res) => {
 //   try {

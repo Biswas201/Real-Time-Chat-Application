5f9bf632ba3c7b5e009898cff2a2c685f1f54167
anurup <anurupbiswas143@gmail.com>
2024-09-15 23:55:43 +0530

    get all users except login user

--- Backend/server.js
+++ Backend/server.js
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoute from "./routes/authRoutes.js";
 import msgRoute from "./routes/msgRoute.js";
+import userRoute from "./routes/userRoute.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cookieParser())
-
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 3000;
 
@@ -30,6 +30,7 @@ app.get("/", (req, res) => {
 //routes
 app.use("/api/auth", authRoute);
 app.use("/api/message", msgRoute);
+app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
@@ -37,12 +38,12 @@ app.listen(PORT, () => {
 
 // error handler
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500
-  const message = err.message || "Internal Server Error"
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
 
   return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
-  })
-})
+  });
+});

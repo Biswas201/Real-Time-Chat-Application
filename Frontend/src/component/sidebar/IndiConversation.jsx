d088742d3385e60e7acacbfa0a2ac1a49071c527
anurup <anurupbiswas143@gmail.com>
2024-09-17 20:25:39 +0530

    changing

--- Frontend/src/component/sidebar/IndiConversation.jsx
+++ Frontend/src/component/sidebar/IndiConversation.jsx
@@ -1,28 +1,24 @@
-import React from 'react'
+import React from 'react';
+import userPic from "../../assets/user.png";
 
 const IndiConversation = () => {
   return (
     <>
-      <div
-        className={`flex gap-2 items-center hover:bg-blue-500 rounded p-2 py-1 cursor-pointer ${
-          isSelected ? "bg-sky-500" : ""
-        }`}
-        onClick={() => setSelectedConversation(conversation)}
-      >
-        <div className={`avatar ${isOnline ? "online" : ""}`}>
+      <div className="flex gap-2 items-center hover:bg-blue-500 rounded p-2 py-1 cursor-pointer">
+        <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img src={conversation.profilePic} alt="" />
+            <img src={userPic} alt="" />
           </div>
         </div>
 
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-slate-700">{conversation.username}</p>
+            <p className="font-bold text-slate-700">Anurup</p>
           </div>
         </div>
       </div>
 
-      {!lastIndex && <div className="divider my-0 py-0 h-1"></div>}
+      <div className="divider my-0 py-0 h-1"></div>
     </>
   )
 }
--- Frontend/src/component/sidebar/Logout.jsx
+++ Frontend/src/component/sidebar/Logout.jsx
@@ -4,14 +4,7 @@ import { BiLogOut } from "react-icons/bi";
 const Logout = () => {
   return (
     <div className="mt-auto">
-      {!loading ? (
-        <BiLogOut
-          className="w-6 h-6 text-red-500 cursor-pointer"
-          onClick={logout}
-        />
-      ) : (
-        <span className="loading loading-spinner"></span>
-      )}
+      <BiLogOut className="w-6 h-6 text-red-500 cursor-pointer"/>
     </div>
   )
 };
